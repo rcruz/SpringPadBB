edf15b796e28db8eb958876bfb4c8b3dbaee334c
Rowell Cruz <r@rowellcruz.com>
2013-02-28 22:43:09 -0500

    Added lib/js/main.js as the main file

--- build/build.js
+++ build/build.js
@@ -8,8 +8,8 @@
         return moduleName === "../../dependencies/almond" ? contents : wrappedContents;
     },
     name: "../../dependencies/almond",
-    include: ["springPad"], // Main file
-    insertRequire: ["springPad"], // Auto-load main file
+    include: ["main"], // Main file
+    insertRequire: ["main"], // Auto-load main file
     out: "../bin/springPad.min.js",
     wrap: true
 }
