8010e638cc9bdb8166bbe962a21b10a33dc8e934
wondo <donnawon@gmail.com>
2013-04-06 17:34:34 -0400

    Completed the screen manager and added some sample screens

--- springPad/lib/js/screenManager.js
+++ springPad/lib/js/screenManager.js
@@ -1,64 +1,65 @@
 var ScreenManager,
     baseUrl = "./screens/",
-    containerDivId = "container-hehau", // example name
+    containerDivId = "container",
     containerDiv;
 
 function downloadPage(url, callback) {
     // Make a "GET" http request to get the screen content text we want 
     var request = new XMLHttpRequest();
-    
+    request.open("GET",url,false);
+    request.send();
+    console.log(request.responseText);
+    return request.responseText;
     // "callback" function will run when everything is ready
 
 }
 
-function loadContentIntoContainerDiv(content) {
-    // Get a reference to the container div using the div id
-    
-    // Clear out the container div
-   
+function loadContentIntoContainerDiv(content) { 
     // Load the contents into the container div 
-    
+    containerDiv.innerHTML = content;
 }
 
 function fadeContainerOut() {
     // Fancy extra
     // Apply some css to the container to fade it out 
+    containerDiv.classList.remove("visible");
+    containerDiv.classList.add("hidden");    
 }
 
 function fadeContainerIn() {
     // Fancy extra
     // Apply some css to the container to fade it in
+    containerDiv.classList.remove("hidden");
+    containerDiv.classList.add("visible");
 }
 
 function loadScreen(screenName, callback) {
     // All the screens are located at the base url
-    var url = baseUrl + screenName; 
+    var url = baseUrl + screenName + ".html"; 
     
-    var fadeThePages = function () {
-        // Fade out the old page
-        // Fade in the new page
-    };
     
     // Give a function to run when the request is done and we have the content
-    downloadPage(url, callbackGoesHere);
     
-
-    var newTitle = "";
-
-    // Later
-    callback(newTitle, a, b, c);
+    var content = downloadPage(url);
+    
+    // Get a reference to the container div using the div id
+    containerDiv = document.getElementById(containerDivId);
+    
+    // Fade out old container first. Takes 200 ms to finish
+    fadeContainerOut();
+    
+    // Do these two things at the same time
+    var doingStuffFunction = function () {
+        loadContentIntoContainerDiv(content);
+        fadeContainerIn();
+    }
+    
+    // Wait 200 ms before doing stuff
+    setTimeout(doingStuffFunction, 200);
 }
-
-// example usage of loadScreen
-// var callback = function (newTitle, a, b, c) {
-//  // This function will run when the loadScreen function is finished loading
-//  Titlebar.setTitleText("new title");
-// }
-//
-// loadScreen("theNote", callback);
 
 ScreenManager = {
     load: loadScreen
 };
 
-module.exports = ScreenManager;
+// module.exports = ScreenManager;
