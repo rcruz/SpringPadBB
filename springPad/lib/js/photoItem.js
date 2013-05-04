c2f70a28e2cdc1e014cdc4dcbd8d76dc4ab217d3
Rowell Cruz <r@rowellcruz.com>
2013-05-03 22:18:58 -0400

    Added notebook photo page and PhotoItem js

---
+++ springPad/lib/js/photoItem.js
@@ -0,0 +1,53 @@
+var PhotoItem,
+    photoImgId = "photoImg",
+    photoImgElement,
+    photoDescId = "photoDesc",
+    photoDescElement,
+    photoDescContentId = "photoDescContent",
+    photoDescContentElement,
+    photoCommentsId = "photoComments",
+    photoCommentsElement;
+
+function init() {
+    photoImgElement = document.getElementById(photoImgId);
+    photoDescElement = document.getElementById(photoDescId);
+    photoDescContentElement = document.getElementById(photoDescContentId);
+    photoCommentsElement = document.getElementById(photoCommentsId);
+}
+
+function setImage(url) {
+   photoImgElement.src = url; 
+}
+   
+function setTitle(title) {
+    // TitleBar exists already, so it can be used
+    TitleBar.setTitleText(title);
+}
+
+function setDescription(text) {
+    photoDescContentElement.innerHTML = text;
+}
+
+function createComment(info) {
+    // Create the comment div
+    var commentDiv = document.createElement("div");
+    commentDiv.classList.add("photoComment");
+    commentDiv.innerHTML = info;
+    
+    // Add the comment div to the end of the comment section
+    photoCommentsElement.appendChild(commentDiv);
+}
+
+function load(info) {
+    init();
+    setImage(info.imageUrl);
+    setTitle(info.title);
+    setDescription(info.description);
+}
+
+PhotoItem = {
+    init: init,
+    load: load
+};
+
+//module.exports = PhotoItem;
