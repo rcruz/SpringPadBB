7673530c9c3d571ef91f7dfcf0c62829b5fa6003
wondo <donnawon@gmail.com>
2013-10-20 15:46:56 -0400

    Resizng Note Items for z10

--- springPad/lib/js/imageResizer.js
+++ springPad/lib/js/imageResizer.js
@@ -1,6 +1,6 @@
 var imageResizer,
-    height = 165,
-    width = 165,
+    height = 354,
+    width = 354,
     baseUrl = "https://springpad.com/api/image/scale?c=sq&w=" + width + "&h=" + height + "&maxw=-1&maxh=-1&url=";
 
 function resizeUrl(imageUrl) {
